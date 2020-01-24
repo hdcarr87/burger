fe68fe9f5be03bb77df72dae487e49c178d6cb3f
hdcarr87 <hdmanley87@gmail.com>
2020-01-23 19:29:58 -0700

    fixedpostroute

--- config/orm.js
+++ config/orm.js
@@ -43,7 +43,7 @@ var orm = {
     },
 //insertOne()
     insertOne: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO" + table;
+        var queryString = "INSERT INTO " + table;
 
         queryString += " (";
         queryString += cols.toString();
@@ -63,7 +63,7 @@ var orm = {
     },
 //updateOne()
     updateOne: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE" + table;
+        var queryString = "UPDATE " + table;
 
         queryString += " SET ";
         queryString += objToSql(objColVals);
